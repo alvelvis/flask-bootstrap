4757aa259f183a43e1e63cba82953c998a5e4c7b
Elvis de Souza <elvis.desouza99@gmail.com>
2021-04-30 22:35:09 -0300

    environment set

---
+++ flask/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+import sys
+import shutil
+import re
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template(
+        'index.html', 
+        title=""
+        )
